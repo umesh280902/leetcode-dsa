1817628f1597839b122143cd93e1e87b6c2ab624
Umesh <umeshkumawat280@gmail.com>
2025-01-16 09:31:49 +0530

    Day 21

---
+++ leetcode/basics/countPrimes.java
@@ -0,0 +1,53 @@
+// 204. Count Primes
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// Given an integer n, return the number of prime numbers that are strictly less than n.
+
+ 
+
+// Example 1:
+
+// Input: n = 10
+// Output: 4
+// Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+// Example 2:
+
+// Input: n = 0
+// Output: 0
+// Example 3:
+
+// Input: n = 1
+// Output: 0
+ 
+
+// Constraints:
+
+// 0 <= n <= 5 * 106
+
+public class countPrimes {
+    class Solution {
+        public int countPrimes(int n) {
+            if(n<=1){
+                return 0;
+            }
+    
+            boolean isPrime[]=new boolean[n+1];
+            Arrays.fill(isPrime,true);
+            isPrime[0]=false;
+            isPrime[1]=false;
+            int count=0;
+            for(int i=2;i<n;i++){
+                if(isPrime[i]){
+                    count++;
+                    for(int j=2*i;j<n;j=j+i){
+                        isPrime[j]=false;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
