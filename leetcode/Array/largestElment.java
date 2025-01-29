0817fdc826d6b2d6dedb45ab16035baaba345082
Umesh <umeshkumawat280@gmail.com>
2025-01-29 18:22:51 +0530

    Day 34

---
+++ leetcode/Array/largestElment.java
@@ -0,0 +1,34 @@
+package leetcode.Array;
+// Largest Element in Array
+// Difficulty: BasicAccuracy: 67.48%Submissions: 405K+Points: 1
+// Given an array arr[]. The task is to find the largest element and return it.
+
+// Examples:
+
+// Input: arr[] = [1, 8, 7, 56, 90]
+// Output: 90
+// Explanation: The largest element of the given array is 90.
+// Input: arr[] = [5, 5, 5, 5]
+// Output: 5
+// Explanation: The largest element of the given array is 5.
+// Input: arr[] = [10]
+// Output: 10
+// Explanation: There is only one element which is the largest.
+// Constraints:
+// 1 <= arr.size()<= 106
+// 0 <= arr[i] <= 106
+public class largestElment {
+    class Solution {
+        public static int largest(int[] arr) {
+            // code here
+            int max=arr[0];
+            int n=arr.length;
+            for(int i=1;i<n;i++){
+                if(arr[i]>max){
+                    max=arr[i];
+                }
+            }
+            return max;
+        }
+    }
+}
