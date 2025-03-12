a6f6831fe867b4d0620a0fed43a44b267909f388
Umesh <umeshkumawat280@gmail.com>
2025-03-12 18:45:09 +0530

    Day 7

---
+++ leetcode/Array/medium/SetMatrixZeros.java
@@ -0,0 +1,73 @@
+package leetcode.Array.medium;
+// 73. Set Matrix Zeroes
+// Solved
+// Medium
+// Topics
+// Companies
+// Hint
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+// You must do it in place.
+
+ 
+
+// Example 1:
+
+
+// Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+// Example 2:
+
+
+// Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+// Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[0].length
+// 1 <= m, n <= 200
+// -231 <= matrix[i][j] <= 231 - 1
+public class SetMatrixZeros {
+    class Solution {
+        public void setZeroes(int[][] matrix) {
+            int col0=1;
+            int n=matrix.length;
+            int m=matrix[0].length;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    if(matrix[i][j]==0){
+    
+                    matrix[i][0]=0;
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }else{
+                        col0=0;
+                    }
+                    }
+                }
+            }
+    
+            for(int i=1;i<n;i++){
+                for(int j=1;j<m;j++){
+                    if(matrix[i][0]==0||matrix[0][j]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+    
+            if(matrix[0][0]==0){
+                for(int j=0;j<m;j++){
+                    matrix[0][j]=0;
+                }
+            }
+    
+            if(col0==0){
+                for(int i=0;i<n;i++){
+                    matrix[i][0]=0;
+                }
+            }
+        }
+    }
+}
